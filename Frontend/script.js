49fcf133d63cc5037c3dae2435a56a91414faad8
jacob642 <jacob.james1322@gmail.com>
2026-09-13 14:53:15 +0100

    script.js now takes security event data and top attacking IPs and is able to display the data as well. Next will be to turn data into a graph and get severity data showing

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,46 @@
 //changing the number of alerts and their values
 
+// fetching the security data from the backend API
+async function loadData() {
+    //collecting data from security events table
+    const alertsResponse = await fetch ("http://127.0.0.1:8003/alerts");
+    const alerts = await alertsResponse.json();
+    //collecting data from IP address table
+    const ipResponse = await fetch ("http://127.0.0.1:8003/IP-addresses")
+    const ips = await ipResponse.json();
+    // logging the two outputs
+    console.log(alerts);
+    console.log(ips);
+    showAlerts(alerts);
+    showIps(ips);
+}
+
+//displaying data on the dashboard-loop through alerts and ips
+function showAlerts(alerts) {
+  const eventsList = document.getElementById("security-events-list");
+
+eventsList.innerHTML = alerts.map((event) => `
+    <div class="security-event-row">
+        <strong>${event.event_type}</strong>
+        <span>${event.severity}</span>
+        <span>${event.source_ip}</span>
+        <time>${event.created_at}</time>
+    </div>
+`).join("");
+}
+//displaying data for from the ip table
+function showIps(ips) {
+  const ipList = document.getElementById("top-attacking-ips-list");
+ipList.innerHTML = ips.map((ip) => `
+    <div class="ip-address-row">
+        <span>${ip.ip_address}</span>
+        <span>${ip.attack_count}</span>
+    </div>
+`).join(""); 
+}
+loadData();
+    
+
 // keeping the severity totals in local variables
 let criticalCount = 0;
 let highCount = 0;
